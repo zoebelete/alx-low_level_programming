da7d1faca264c04e37e53cb375c3281210ac5f6f
zoebelete <hiwotbelete04@gmai.com>
2022-03-10 11:30:10 -0800

    hello printf

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return:Zero on success
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - Prints quote
+ *
+ * Return: Zero on success
+ *
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
