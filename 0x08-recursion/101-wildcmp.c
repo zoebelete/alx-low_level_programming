6dcd2e715904ea3f801b891fdfd3e40a85cea204
zoebelete <hiwotbelete04@gmail.com>
2022-03-30 05:19:08 -0700

    compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,27 +1,21 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * wildcmp - Funtion
- *
- * @s1: Variable
- * @s2: Variable
- *
- * Return: Int
+ * wildcmp - checks if s1 and s2 matches
+ * @s1: string to check
+ * @s2: string to matches
+ * Return: 1 if matches otherwise 0
  */
 int wildcmp(char *s1, char *s2)
 {
-
-	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-
 	if (*s2 == '*')
 	{
-		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
-			return (1);
-		if (*(s1 + 1) != '\0' && *s2 == '\0')
-			return (0);
+		if (*s1 == '\0' || *(s2 + 1) == '*')
+			return (wildcmp(s1, s2 + 1));
+		else if (wildcmp(s1, s2 + 1))
+			return (wildcmp(s1, s2 + 1));
+		return (wildcmp(s1 + 1, s2));
 	}
-	return (0);
+	else if (*s2 == '\0')
+		return (*s1 == '\0');
+	return (*s1 == *s2 && wildcmp(s1 + 1, s2 + 1));
 }
