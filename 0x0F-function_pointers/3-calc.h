59047378683b33bc09d956afa89a03c30c6da096
zoebelete <hiwotbelete04@gmail.com>
2022-04-14 05:00:52 -0700

    multiple

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,16 @@
-#ifndef TASK3
-#define TASK3
+#ifndef CALC_H
+#define CALC_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * struct op - Struct op
  *
- * @op: operator
- * @f: function
+ * @op: The operator
+ * @f: The function associated
  */
+
 typedef struct op
 {
 	char *op;
@@ -18,5 +23,6 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
+
 
 #endif
