0f46242dcf012b39a687420e600ae5674ff5166b
zoebelete <hiwotbelete04@gmail.com>
2022-04-21 12:46:15 -0700

    linked lists

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,11 +1,19 @@
-        global	main
-	extern	printf
+section .data
+hello:	db "Hello, Holberton",10,0
+format:	db "%s",0
 
-	section	.text
+    section .text
+    global main
+    extern printf
+
 main:
-	mov	rdi, format
-	mov	rax, 0
-	call	printf
-	ret
-format:
-	db "Hello, Holberton", 10, 0
+    push rbp
+    mov rbp, rsp
+
+    mov rsi, hello
+    mov rdi, format
+    call printf
+
+    pop rbp
+    mov rax, 0
+    ret
